e224197da54ce94a6aba72ce141dc0de379cd59d
Xushi8 <111222uuufa@gmail.com>
2024-12-02 21:41:24 +0800

    AF_INET6

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@ int main()
 {
     int port = 7777;
     /* 创建监听socket文件描述符 */
-    int listenfd = socket(PF_INET6, SOCK_STREAM, 0);
+    int listenfd = socket(AF_INET6, SOCK_STREAM, 0);
     /* 创建监听socket的TCP/IP的IPV6 socket地址 */
     struct sockaddr_in address;
     bzero(&address, sizeof(address));
-    address.sin_family = AF_INET;
+    address.sin_family = AF_INET6;
     address.sin_addr.s_addr = htonl(INADDR_ANY); /* INADDR_ANY：将套接字绑定到所有可用的接口 */
     address.sin_port = htons(port);
 
