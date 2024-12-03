975ef2d558ed11d4520b57f7816c9e6f70cd5a4c
Xushi8 <111222uuufa@gmail.com>
2024-12-03 13:07:30 +0800

    locale

--- src/server.c
+++ src/server.c
@@ -1,6 +1,22 @@
 #include <epoll_learn/network/server1.h>
 
+#include <locale.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main(void)
 {
+#ifdef _WIN32
+    // -finput-charset=utf-8 -fexec-charset=utf-8
+    setlocale(LC_ALL, ".UTF-8");
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+#elif defined __APPLE__
+    setlocale(LC_ALL, "UTF-8");
+#elif defined __linux__
+    setlocale(LC_ALL, "C.UTF-8");
+#endif
+
     server1();
 }
