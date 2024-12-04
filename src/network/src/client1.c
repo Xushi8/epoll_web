6759161bf574de5fecc0b35468983727be9c7536
Xushi8 <111222uuufa@gmail.com>
2024-12-04 14:52:32 +0800

    client over

--- src/network/src/client1.c
+++ src/network/src/client1.c
@@ -12,19 +12,41 @@
 void client1(void)
 {
     // 1. 获取服务器地址信息
-    struct addrinfo hints = {}, *res;
-    int ret;
+    struct addrinfo hints = {};
+    int check_error;
 
     // 设置 hints
-    hints.ai_family = AF_INET6;      // IPv6
-    hints.ai_socktype = SOCK_STREAM; // TCP
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
 
     // 使用 getaddrinfo 获取服务器的地址信息
-    ret = getaddrinfo("localhost", "10000", &hints, &res);
-    if (ret != 0)
+    struct addrinfo* res;
+    check_error = getaddrinfo("localhost", "10000", &hints, &res);
+    if (check_error != 0)
     {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ret));
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(check_error));
         exit(1);
+    }
+
+    // 处理返回的 IPv6 地址
+    struct addrinfo* p;
+    for (p = res; p != NULL; p = p->ai_next)
+    {
+        // 只会遍历 IPv6 地址
+        if (p->ai_family == AF_INET6)
+        {
+            char addr_str[INET6_ADDRSTRLEN];
+            struct sockaddr_in6* ipv6 = (struct sockaddr_in6*)p->ai_addr;
+            inet_ntop(AF_INET6, &(ipv6->sin6_addr), addr_str, sizeof(addr_str));
+            printf("IPv6 address: %s\n", addr_str);
+        }
+        else
+        {
+            char addr_str[INET_ADDRSTRLEN];
+            struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
+            inet_ntop(AF_INET, &(ipv4->sin_addr), addr_str, sizeof(addr_str));
+            printf("IPv4 address: %s\n", addr_str);
+        }
     }
 
     // 2. 创建套接字
@@ -37,8 +59,8 @@ void client1(void)
     }
 
     // 3. 连接服务器
-    ret = connect(fd, res->ai_addr, res->ai_addrlen);
-    if (ret == -1)
+    check_error = connect(fd, res->ai_addr, res->ai_addrlen);
+    if (check_error == -1)
     {
         perror("connect");
         close(fd);
