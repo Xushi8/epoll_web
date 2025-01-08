f8f844263f18127493657f4aa73e6a78012fbc82
Xushi8 <111222uuufa@gmail.com>
2025-01-08 18:36:04 +0800

    add game

---
+++ src/game/include/epoll_web/game/snack.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <epoll_web/common/common.hpp>
+
+EPOLL_WEB_BEGIN_NAMESPACE
+
+EPOLL_WEB_END_NAMESPACE
--- src/test.cpp
+++ src/test.cpp
@@ -1,28 +1,33 @@
 #include <ncurses.h>
+#include <vector>
 
 int main()
 {
+    // 初始化 ncurses
     initscr();
+    noecho();
+    curs_set(0);
 
-    int scrLine, scrCol;
-    getmaxyx(stdscr, scrLine, scrCol);     // 获取标准屏幕的行/列数
-    move((scrLine / 2) - 1, (scrCol / 2) - 1); // 将光标移至屏幕中央
-    printw("Hello World!");
+    // 示例二维数组的坐标点
+    std::vector<std::pair<int, int>> points = {
+        {1, 1}, {2, 2}, {3, 3}, {4, 4},
+        {5, 5}, {6, 6}, {7, 7}, {8, 8}, {9, 9},
+        {10, 10}, {11, 11}, {12, 12}, {13, 13}, {14, 14}};
 
-    WINDOW* upwin = subwin(stdscr, scrLine / 2, scrCol, 0, 0);
-    WINDOW* downwin = subwin(stdscr, scrLine / 2, scrCol, scrLine / 2, 0);
+    // 遍历坐标数组并打印每个点
+    for (const auto& point : points)
+    {
+        int x = point.first;  // 获取 x 坐标
+        int y = point.second; // 获取 y 坐标
+        mvaddch(y, x, '*');   // 将点绘制在 (x, y) 位置
+    }
 
-    box(upwin, '|', '+');
-    box(downwin, '|', '+');
-    touchwin(stdscr);
+    // 刷新屏幕以显示绘制的内容
+    refresh();
 
-    wmove(downwin, 1, 1);
-    // refresh();
-    int a;
-    wscanw(downwin, "%d", &a);
-
+    // 等待按键退出
     getch();
-
-    // 清理并退出 ncurses 环境
     endwin();
+
+    return 0;
 }
