2e9b37f203dc80e0ecb3e120c340c8b3a6ea0b3b
Xushi8 <111222uuufa@gmail.com>
2025-01-09 13:47:43 +0800

    read over

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -11,11 +11,6 @@ find_package(NCURSES REQUIRED)
 # dynamic link begin
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".dll" ".so")
 
-if(EPOLL_WEB_TEST)
-    find_package(benchmark REQUIRED)
-    find_package(GTest REQUIRED)
-endif()
-
 if(EPOLL_WEB_STATIC_EXTERNAL_LIB)
     set(CMAKE_FIND_LIBRARY_SUFFIXES ".lib" ".a")
 endif()
